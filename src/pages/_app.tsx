45c912722c254f420bd772a548864b931daf687f
scinscinscin <scinorandex1@gmail.com>
2024-02-15 06:06:37 +0800

    add meta and html lang

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { NextSeo } from "next-seo";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>Lance Owen Gulinao's Portfolio</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
+
+      <NextSeo
+        title="Lance Owen Gulinao's Portfolio"
+        openGraph={{
+          title: "Lance Owen Gulinao's Portfolio",
+          images: [{ url: "https://scinscinscin.github.io/meta.png" }],
+        }}
+      />
 
       <Component {...pageProps} />
     </>
